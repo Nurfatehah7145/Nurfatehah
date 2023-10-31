1ad254c2765140ad3f5deb9d20b7affda84e57ef
Nurfatehah7145 <147596928+Nurfatehah7145@users.noreply.github.com>
2023-10-31 17:33:18 +0700

    Update tugas1alpro.py

--- tugas1alpro.py
+++ tugas1alpro.py
@@ -4,3 +4,32 @@
 3. Nur Fatehah 23091397160
 """
 
+#Daftar Menu
+
+print(
+"""ter BBQ : 43_637
+    2. Meat Mo
+    1. Frankfurnsta : 43_637
+    3. Super Supreme : 43_637
+    4. Super Supreme Chicken : 43_637
+""")
+
+harga_pizza = 0
+topping_pizza = int(input("pilih topping pizza: ").lower())
+if topping_pizza == 1:
+    topping_pizza = "frankfurter"
+    harga_pizza = 43_637
+elif topping_pizza == 2:
+    topping_pizza = "Meat Monsta"
+    harga_pizza = 43_637
+elif topping_pizza == 3:
+    topping_pizza = "Super Supreme"
+    harga_pizza = 43_637
+elif topping_pizza == 4:
+    topping_pizza = "Super Supreme chicken"
+    harga_pizza = 43_637
+
+#Crust pizza
+print(
+"""
+    1.Pan : free
